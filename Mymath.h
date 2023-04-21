b5e79c3da6e2cfa834daddf559a690b4ebefe247
TonosakiRen <ren221603@gmail.com>
2023-04-21 11:47:52 +0900

    MT3_00_03

--- Mymath.h
+++ Mymath.h
@@ -6,7 +6,7 @@
 #include "Vector4.h"
 #include "Matrix4x4.h"
 #include <Novice.h>
-
+#include <assert.h>
 
 #pragma region Vector3
 //Vetor3
@@ -286,3 +286,67 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* messa
 }
 #pragma endregion
 
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = 1;
+	tmp.m[0][1] = 0;
+	tmp.m[0][2] = 0;
+	tmp.m[0][3] = 0;
+
+	tmp.m[1][0] = 0;
+	tmp.m[1][1] = 1;
+	tmp.m[1][2] = 0;
+	tmp.m[1][3] = 0;
+
+	tmp.m[2][0] = 0;
+	tmp.m[2][1] = 0;
+	tmp.m[2][2] = 1;
+	tmp.m[2][3] = 0;
+
+	tmp.m[3][0] = translate.x;
+	tmp.m[3][1] = translate.y;
+	tmp.m[3][2] = translate.z;
+	tmp.m[3][3] = 1;
+
+	return tmp;
+
+}
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = scale.x;
+	tmp.m[0][1] = 0;
+	tmp.m[0][2] = 0;
+	tmp.m[0][3] = 0;
+
+	tmp.m[1][0] = 0;
+	tmp.m[1][1] = scale.y;
+	tmp.m[1][2] = 0;
+	tmp.m[1][3] = 0;
+
+	tmp.m[2][0] = 0;
+	tmp.m[2][1] = 0;
+	tmp.m[2][2] = scale.z;
+	tmp.m[2][3] = 0;
+
+	tmp.m[3][0] = 0;
+	tmp.m[3][1] = 0;
+	tmp.m[3][2] = 0;
+	tmp.m[3][3] = 1;
+
+	return tmp;
+}
+Vector3 Transform(const Vector3& vector,const Matrix4x4& matrix) {
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
+	return result;
+
+}
