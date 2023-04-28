57a88069c9d22f30b2b02d84e5ff989920ce798d
TonosakiRen <ren221603@gmail.com>
2023-04-28 11:24:14 +0900

    MT3_01_01

--- Mymath.h
+++ Mymath.h
@@ -62,20 +62,29 @@ inline Vector3 Normalize(const Vector3& v) {
 	tmp = sqrtf(tmp);
 	return { v.x / tmp, v.y / tmp, v.z / tmp };
 }
-
 static const int kColumnWidth = 60;
 static const int kRowHeight = 20;
-
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
 	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
 	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
 	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
 	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
+inline Vector3& operator+=(Vector3& v1, const Vector3& v2) {
+	v1.x += v2.x;
+	v1.y += v2.y;
+	v1.z += v2.z;
+	return v1;
+}
+inline Vector3& operator-=(Vector3& v1, const Vector3& v2) {
+	v1.x -= v2.x;
+	v1.y -= v2.y;
+	v1.z -= v2.z;
+	return v1;
+}
 
 #pragma endregion
-
-#pragma region Matrix3x3
+#pragma region Matrix4x4
 Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 tmp;
 	tmp.m[0][0] = m1.m[0][0] + m2.m[0][0];
@@ -97,7 +106,47 @@ Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
 	return tmp;
 
 }
+Matrix4x4 operator +(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = m1.m[0][0] + m2.m[0][0];
+	tmp.m[0][1] = m1.m[0][1] + m2.m[0][1];
+	tmp.m[0][2] = m1.m[0][2] + m2.m[0][2];
+	tmp.m[0][3] = m1.m[0][3] + m2.m[0][3];
+	tmp.m[1][0] = m1.m[1][0] + m2.m[1][0];
+	tmp.m[1][1] = m1.m[1][1] + m2.m[1][1];
+	tmp.m[1][2] = m1.m[1][2] + m2.m[1][2];
+	tmp.m[1][3] = m1.m[1][3] + m2.m[1][3];
+	tmp.m[2][0] = m1.m[2][0] + m2.m[2][0];
+	tmp.m[2][1] = m1.m[2][1] + m2.m[2][1];
+	tmp.m[2][2] = m1.m[2][2] + m2.m[2][2];
+	tmp.m[2][3] = m1.m[2][3] + m2.m[2][3];
+	tmp.m[3][0] = m1.m[3][0] + m2.m[3][0];
+	tmp.m[3][1] = m1.m[3][1] + m2.m[3][1];
+	tmp.m[3][2] = m1.m[3][2] + m2.m[3][2];
+	tmp.m[3][3] = m1.m[3][3] + m2.m[3][3];
+	return tmp;
+}
 Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = m1.m[0][0] - m2.m[0][0];
+	tmp.m[0][1] = m1.m[0][1] - m2.m[0][1];
+	tmp.m[0][2] = m1.m[0][2] - m2.m[0][2];
+	tmp.m[0][3] = m1.m[0][3] - m2.m[0][3];
+	tmp.m[1][0] = m1.m[1][0] - m2.m[1][0];
+	tmp.m[1][1] = m1.m[1][1] - m2.m[1][1];
+	tmp.m[1][2] = m1.m[1][2] - m2.m[1][2];
+	tmp.m[1][3] = m1.m[1][3] - m2.m[1][3];
+	tmp.m[2][0] = m1.m[2][0] - m2.m[2][0];
+	tmp.m[2][1] = m1.m[2][1] - m2.m[2][1];
+	tmp.m[2][2] = m1.m[2][2] - m2.m[2][2];
+	tmp.m[2][3] = m1.m[2][3] - m2.m[2][3];
+	tmp.m[3][0] = m1.m[3][0] - m2.m[3][0];
+	tmp.m[3][1] = m1.m[3][1] - m2.m[3][1];
+	tmp.m[3][2] = m1.m[3][2] - m2.m[3][2];
+	tmp.m[3][3] = m1.m[3][3] - m2.m[3][3];
+	return tmp;
+}
+Matrix4x4 operator -(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 tmp;
 	tmp.m[0][0] = m1.m[0][0] - m2.m[0][0];
 	tmp.m[0][1] = m1.m[0][1] - m2.m[0][1];
@@ -140,7 +189,6 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 	tmp.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
 	return tmp;
 }
-
 Matrix4x4 operator *(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 tmp;
 	tmp.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
@@ -164,7 +212,6 @@ Matrix4x4 operator *(const Matrix4x4& m1, const Matrix4x4& m2) {
 	tmp.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
 	return tmp;
 }
-
 Matrix4x4 Inverse(const Matrix4x4& m) {
 	float lal = m.m[0][0] * m.m[1][1] * m.m[2][2] * m.m[3][3]
 		+ m.m[0][0] * m.m[1][2] * m.m[2][3] * m.m[3][1]
@@ -300,7 +347,6 @@ Matrix4x4 MakeIdentity4x4() {
 
 	return tmp;
 }
-
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* message) {
 	Novice::ScreenPrintf(x, y, message);
 	for (int row = 0; row < 4; ++row) {
@@ -309,8 +355,6 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* messa
 		}
 	}
 }
-#pragma endregion
-
 Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
 	Matrix4x4 tmp;
 	tmp.m[0][0] = 1;
@@ -360,7 +404,7 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 
 	return tmp;
 }
-Vector3 Transform(const Vector3& vector,const Matrix4x4& matrix) {
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 	Vector3 result;
 	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
 	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
@@ -375,7 +419,6 @@ Vector3 Transform(const Vector3& vector,const Matrix4x4& matrix) {
 	return result;
 
 }
-
 Matrix4x4 MakeRotateXMatrix(float radian) {
 
 	Matrix4x4 tmp;
@@ -402,7 +445,6 @@ Matrix4x4 MakeRotateXMatrix(float radian) {
 
 	return tmp;
 }
-
 Matrix4x4 MakeRotateYMatrix(float radian) {
 
 	Matrix4x4 tmp;
@@ -429,7 +471,6 @@ Matrix4x4 MakeRotateYMatrix(float radian) {
 
 	return tmp;
 }
-
 Matrix4x4 MakeRotateZMatrix(float radian) {
 
 	Matrix4x4 tmp;
@@ -456,7 +497,6 @@ Matrix4x4 MakeRotateZMatrix(float radian) {
 
 	return tmp;
 }
-
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
 	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
 	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
@@ -468,3 +508,115 @@ Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Ve
 
 	return tmp;
 }
+inline Matrix4x4& operator*=(Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] +
+		m1.m[0][3] * m2.m[3][0];
+	tmp.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] +
+		m1.m[0][3] * m2.m[3][1];
+	tmp.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] +
+		m1.m[0][3] * m2.m[3][2];
+	tmp.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] +
+		m1.m[0][3] * m2.m[3][3];
+
+	tmp.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] +
+		m1.m[1][3] * m2.m[3][0];
+	tmp.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] +
+		m1.m[1][3] * m2.m[3][1];
+	tmp.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] +
+		m1.m[1][3] * m2.m[3][2];
+	tmp.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] +
+		m1.m[1][3] * m2.m[3][3];
+
+	tmp.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] +
+		m1.m[2][3] * m2.m[3][0];
+	tmp.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] +
+		m1.m[2][3] * m2.m[3][1];
+	tmp.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] +
+		m1.m[2][3] * m2.m[3][2];
+	tmp.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] +
+		m1.m[2][3] * m2.m[3][3];
+
+	tmp.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] +
+		m1.m[3][3] * m2.m[3][0];
+	tmp.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] +
+		m1.m[3][3] * m2.m[3][1];
+	tmp.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] +
+		m1.m[3][3] * m2.m[3][2];
+	tmp.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] +
+		m1.m[3][3] * m2.m[3][3];
+	m1 = tmp;
+	return m1;
+
+	
+}
+
+// 透視投影行列
+inline Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 tmp;
+	float cot = 1.0f / std::tan(fovY * 0.5f);
+	tmp.m[0][0] = 1.0f / aspectRatio * cot;
+	tmp.m[0][1] = 0;
+	tmp.m[0][2] = 0;
+	tmp.m[0][3] = 0;
+	tmp.m[1][0] = 0;
+	tmp.m[1][1] = cot;
+	tmp.m[1][2] = 0;
+	tmp.m[1][3] = 0;
+	tmp.m[2][0] = 0;
+	tmp.m[2][1] = 0;
+	tmp.m[2][2] = farClip / (farClip - nearClip);
+	tmp.m[2][3] = 1.0f;
+	tmp.m[3][0] = 0;
+	tmp.m[3][1] = 0;
+	tmp.m[3][2] = -nearClip * farClip / (farClip - nearClip);
+	tmp.m[3][3] = 0;
+
+	return tmp;
+}
+//正射影行列
+inline Matrix4x4 MakeOrthograohicmatrix(float left, float top, float right, float bottom,float nearClip,float farClip) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = 2.0f / (right - left);
+	tmp.m[0][1] = 0;
+	tmp.m[0][2] = 0;
+	tmp.m[0][3] = 0;
+	tmp.m[1][0] = 0;
+	tmp.m[1][1] = 2.0f / (top - bottom);
+	tmp.m[1][2] = 0;
+	tmp.m[1][3] = 0;
+	tmp.m[2][0] = 0;
+	tmp.m[2][1] = 0;
+	tmp.m[2][2] = 1.0f / (farClip - nearClip);
+	tmp.m[2][3] = 0;
+	tmp.m[3][0] = (left + right) / (left - right);
+	tmp.m[3][1] = (top + bottom) / (bottom - top);
+	tmp.m[3][2] = nearClip / (nearClip - farClip);
+	tmp.m[3][3] = 1.0f;
+
+	return tmp;
+
+}
+//ビューポート変換行列
+inline Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height,float minDepth , float maxDepth) {
+	Matrix4x4 tmp;
+	tmp.m[0][0] = width / 2.0f;
+	tmp.m[0][1] = 0;
+	tmp.m[0][2] = 0;
+	tmp.m[0][3] = 0;
+	tmp.m[1][0] = 0;
+	tmp.m[1][1] = -height / 2.0f;
+	tmp.m[1][2] = 0;
+	tmp.m[1][3] = 0;
+	tmp.m[2][0] = 0;
+	tmp.m[2][1] = 0;
+	tmp.m[2][2] = maxDepth - minDepth;
+	tmp.m[2][3] = 0;
+	tmp.m[3][0] = left + (width / 2.0f);
+	tmp.m[3][1] = top  + (height / 2.0f);
+	tmp.m[3][2] = minDepth;
+	tmp.m[3][3] = 1;
+	return tmp;
+}
+#pragma endregion
+
